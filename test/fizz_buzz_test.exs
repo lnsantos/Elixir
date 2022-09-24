cd3f4a74cdb17839686714ef1b354be1088ca0b6
Lucas santos <=>
2022-09-24 03:38:53 -0300

    feat: added basic test

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,17 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  test "when process file then result must be list numbers with fizz and fizzbuzz" do 
+    expected = {:ok, [1, :fizz, :fizzbuzz]}
+    actual = FizzBuzz.get_file_content("./test/resources/fizzbuzz/valid.txt")
+
+    assert actual == expected
   end
+
+  test "when process file not valid then result must be error message" do 
+    expected = {:error, "Error in read file: enoent"}
+    actual = FizzBuzz.get_file_content("./test/resources/fizzbuzz/invalid.txt")
+
+    assert actual == expected
+  end 
 end
