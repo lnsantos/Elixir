339416a5a9a5fb7e376bde1aff851040b371a466
Lucas santos <=>
2022-09-24 02:58:40 -0300

    feat: create basic example guards in elixir

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -2,12 +2,24 @@ defmodule FizzBuzz do
   
   def get_file_content(filename), do: filename |> File.read() |> transform_list_number()
 
-
   def transform_list_number({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_from_string_to_numbers/1)
   end 
   def transform_list_number({:error, reason}), do: "Error in read file : #{reason}" 
+
+  def convert_from_string_to_numbers(result) do
+    result
+    |> String.to_integer()
+    |> convert_evaluates()
+  end 
+
+  def convert_evaluates(number), do: number |> validate_value()
+
+  def validate_value(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def validate_value(number) when rem(number, 3) == 0, do: :fizz
+  def validate_value(number) when rem(number, 5) == 0, do: :buzz
+  def validate_value(number), do: number 
 
 end
