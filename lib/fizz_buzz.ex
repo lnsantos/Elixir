21356e619b4989f46c56b7e41906bd0ea32d90ca
Lucas santos <=>
2022-09-24 03:06:36 -0300

    feat: added private function in elixir :3

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -2,7 +2,7 @@ defmodule FizzBuzz do
   
   def get_file_content(filename), do: filename |> File.read() |> transform_list_number()
 
-  def transform_list_number({:ok, result}) do
+  defp transform_list_number({:ok, result}) do
     data = result
     |> String.split(",")
     |> Enum.map(&convert_from_string_to_numbers/1)
@@ -10,7 +10,7 @@ defmodule FizzBuzz do
     {:ok, data}
   end 
 
-  def transform_list_number({:error, reason}), do: {:error, "Error in read file: #{reason}"}
+  defp transform_list_number({:error, reason}), do: {:error, "Error in read file: #{reason}"}
 
   def convert_from_string_to_numbers(result) do
     result
@@ -18,11 +18,11 @@ defmodule FizzBuzz do
     |> convert_evaluates()
   end 
 
-  def convert_evaluates(number), do: number |> validate_value()
+  defp convert_evaluates(number), do: number |> validate_value()
 
-  def validate_value(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
-  def validate_value(number) when rem(number, 3) == 0, do: :fizz
-  def validate_value(number) when rem(number, 5) == 0, do: :buzz
-  def validate_value(number), do: number 
+  defp validate_value(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp validate_value(number) when rem(number, 3) == 0, do: :fizz
+  defp validate_value(number) when rem(number, 5) == 0, do: :buzz
+  defp validate_value(number), do: number 
 
 end
